1193f47a0d30c6070e5983d818b34a223f25b572
hotsuma kawashima <hkawashimajob1992@gmail.com>
2020-05-27 16:18:02 -0700

    timezone source

---
+++ ios-clock/SelectTimezoneViewController.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+class SelectTimezoneViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+	
+	@IBOutlet var tableView: UITableView? = nil
+	@IBOutlet var searchBar: UISearchBar? = nil
+	
+	let timezones = TimeZone.knownTimeZoneIdentifiers.sorted()
+	var filtered = [String]()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		filtered = timezones
+	}
+	
+	func searchBar(_ searchBar: UISearchBar, textDidChange: String) {
+        if !textDidChange.isEmpty {
+            filtered = timezones.filter {
+                ($0 as NSString).range(of: textDidChange, options: .caseInsensitive).location != NSNotFound
+            }
+        } else {
+            filtered = timezones
+        }
+        tableView?.reloadData()
+	}
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let indexPath = tableView.indexPathForSelectedRow, filtered.indices.contains(indexPath.row) {
+            //Document.shared.addTimezone(filtered[indexPath.row])
+        }
+        self.navigationController?.popViewController(animated: true)
+    }
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return filtered.count
+	}
+
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "timezone", for: indexPath)
+		cell.textLabel!.text = filtered[indexPath.row]
+		return cell
+	}
+
+}
+
